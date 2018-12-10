55669d0f95393b4b640870897e0f1c0f2330965d
Andre Tadeu de Carvalho <andre.tadeu.de.carvalho@gmail.com>
2018-12-10 13:56:32 -0200

    Docker image now uses alpine:3.8 as base image. jlink command excludes man pages and header files.

--- 9-jmod-jlink/run-jlink.sh
+++ 9-jmod-jlink/run-jlink.sh
@@ -4,6 +4,8 @@ if [ "$#" -ge 1 ]
 then
     rm -rf target/dist
 
-    jlink --module-path $1/jmods/:target/jmods --add-modules org.astro,com.andretc83.greetings \
---launcher runapp=com.andretc83.greetings --output target/dist
+    jlink --module-path $1/jmods/:target/jmods \
+--add-modules org.astro,com.andretc83.greetings \
+--launcher runapp=com.andretc83.greetings --no-header-files --no-man-pages \
+--output target/dist
 fi
